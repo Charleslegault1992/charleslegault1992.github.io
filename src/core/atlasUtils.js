7a7b410a86ce2a2da11717dbc61fc05d13b26482
charles legault <charleslegault1992@gmail.com>
2026-09-03 13:36:42 -0400

    0.3461

--- src/core/atlasUtils.js
+++ src/core/atlasUtils.js
@@ -1,7 +1,5 @@
 import { ATLAS_CELL_SIZE, ATLAS_PADDING } from "./gameConstants.js";
 
-import { ATLAS_CELL_SIZE, ATLAS_PADDING } from "./gameConstants.js";
-
 export const getAtlasSource = (col, row, spriteSize, options = {}) => {
   const cellSize = Number.isFinite(options.cellSize) ? options.cellSize : ATLAS_CELL_SIZE;
   const padding = Number.isFinite(options.padding) ? options.padding : ATLAS_PADDING;
